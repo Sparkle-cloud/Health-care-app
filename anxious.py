34ef3d985603dbd0cb4a5738874e0419cb150432
SHARON JOANNA <64791375+Sparkle-cloud@users.noreply.github.com>
2021-02-05 22:33:43 +0530

    Backend codes
    these are the files and codes of The button onclick events. it contains HTML, CSS, Javascript, Python. and also the images and GIFs used.

---
+++ anxious.py
@@ -0,0 +1,6 @@
+import webbrowser
+import os
+
+#Change path to reflect file location
+filename = "file:///C:/Users/sharo/Desktop/python project/testing python/anxious.html"
+webbrowser.open_new_tab(filename)
